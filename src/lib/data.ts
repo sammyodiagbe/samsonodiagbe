cabba054c1ad5db15ea23b224036fe83606da4a2
Samson Odiagbe <sam.odiagbe.dev@gmail.com>
2024-04-29 07:50:37 -0400

    adding url path to data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -4,88 +4,103 @@ export const portfolioData = [
     img: "rafiki.svg",
     detail:
       "Curiosity made me build this app, I was watching a video on youtube where the youtuber was talking about openai API, and voila i decided to build something like chatgpt, I learnt a lot from working with API, to understanding layouts with tailwindcss.",
+    url: "https://rafiki-mbxtalhqi-sammyodiagbes-projects.vercel.app/",
   },
   {
     name: "Bookmark Landing Page",
     img: "bookmark.svg",
     detail:
       "This was one of my first challenges on frontendmentor, I learnt a lot from responsive designs to how to move elements around, and how to layout a page, it was a very fun project.",
+    url: "https://sammyodiagbe.github.io/responsive-bookmark/",
   },
   {
     name: "Github User Search App",
     img: "github.svg",
     detail:
       "In this project, I learnt how to use the GitHub users API to pull profile data and display it. It's a great challenge if you're looking to practice working with a 3rd-party API.",
+    url: "https://devfinder-two.vercel.app/",
   },
   {
     name: "Loop Studio Landing Page",
     img: "business_landing.svg",
     detail:
       "This project challenged my understanding of css grid and how to place elements in the DOM (Absolute, Relative, Fixed), it was a fun project, really helped me strengthen my CSS knowledge",
+    url: "https://sammyodiagbe.github.io/Loopstudios-landing-page/",
   },
   {
     name: "Connect Four Game",
     img: "connect_four.svg",
     detail:
       "My JS knowledge was  challenged in this fun project, I had to  build a two-player game. I wrote the logic for the game and how to test for win conditions. I really had fun building this one, it was a tough one but you know what I finished it.",
+    url: "https://connect-four-41z45w5yw-sammyodiagbe.vercel.app/",
   },
   {
     name: "Dictionary app",
     img: "dictionary.svg",
     detail:
       "In this project, I learnt how to integrate with the Dictionary API to create a real-world dictionary web app. Additional tests include colour themes and font selection, and the ability to listen to some words",
+    url: "https://brainbook.vercel.app/",
   },
   {
     name: "Feature Feedback app",
     img: "feedback_app.svg",
     detail:
       "This is perfect portfolio piece! I learnt how to work with JSON, managing state, and building like a real-world app. It was a really fun project for me.",
+    url: "https://feedbakiie.vercel.app/",
   },
   {
-    name: "Movie App",
+    name: "Entertainment web App",
     img: "movie_app.svg",
     detail: "Some message would go in here",
+    url: "https://entertainment-web-app-rho.vercel.app/",
   },
   {
     name: "Pricing Component",
     img: "pricing_component.svg",
     detail:
       "In this project, I built out an interactive pricing component complete with custom range input slider and pricing toggle. A perfect way to test my JS skills!",
+    url: "https://sammyodiagbe.github.io/interactive-pricing-component/",
   },
   {
-    name: "Pricing Component",
+    name: "Order Summary Component",
     img: "pricing.svg",
     detail:
       "A perfect project for newbies like myself when i started out learning to code frontend websites and web apps also for those who are starting to build confidence with layouts!",
+    url: "https://order-summary-component-cddosuomw-sammyodiagbe.vercel.app/",
   },
   {
     name: "Single-Page Design Landing Page",
     img: "single_landing.svg",
     detail:
       "This project really tested my layout skills, especially in making the site fully responsive. The skills grid was a perfect chance to practice CSS Grid.",
+    url: "https://single-page-design-portfolio-one.vercel.app/",
   },
   {
-    name: "Skilled Landing",
+    name: "Skilled Landing Page",
     img: "skilled_landing.svg",
     detail:
       "This responsive page was a perfect place to start after learning  the fundamentals of HTML & CSS, completed a couple of smaller projects, and want to take it up a notch. This was part of my first projects on frontendmentor",
+    url: "https://skilled-elearning-landng-page.vercel.app/",
   },
   {
     name: "Tips Calculator",
     img: "tips.svg",
     detail:
       "This small app is perfect for me starting to get to grips with JavaScript. The calculator functionality tested how i could use my javascript skills to maniplute the DOM",
+    url: "https://sammyodiagbe.github.io/tip-calculator/",
   },
   {
     name: "Todo app",
     img: "todo_app.svg",
     detail:
       "The classic todo app with a few twists! This app includes a dark/light theme toggle and drag & drop reordering for anyone wanting an extra test.",
+    url: "https://next-todo-app-rouge.vercel.app/",
   },
   {
     name: "News Home Page",
     img: "web3.svg",
     detail:
       "This news homepage was  an excellent opportunity to practice my CSS Grid skills. There were a lots of tricky decisions to make and plenty of learning opportunities!",
+    url: "https://news-homepage-teal-tau.vercel.app/",
   },
 ];
