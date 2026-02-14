132a43a1706d68f9ecacd6c560b338e92dacf445
Samson <sam.odiagbe.dev@gmail.com>
2026-02-13 18:37:02 -0600

    changed resume to updated one

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -80,7 +80,7 @@ export default function Hero() {
             >
               <MagneticButton
                 as="a"
-                href="/docs/Samson_Odiagbe_Software_Developer_Resume.docx"
+                href="/docs/Samson_Odiagbe_Software_Developer_Resume (1).docx"
                 target="_blank"
                 rel="noopener noreferrer"
               >
